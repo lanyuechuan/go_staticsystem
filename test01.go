4cde1bf928bcae6f4c410f60b45618b4e2b32bf2
lanyuechuan <lanyc_go@163.com>
2021-01-06 14:24:03 +0800

    fix: 测试触发器

--- test01.go
+++ test01.go
@@ -2,7 +2,7 @@ package main
 import "net/http"
 
 func main() {
-    fs := http.FileServer(http.Dir("/tmp"))
+    fs := http.FileServer(http.Dir("../"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     http.ListenAndServe(":8000", nil)
